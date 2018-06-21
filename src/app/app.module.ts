7f6ec62981b12bc4d3095417477707b5ab1eaf62
Petr Fedchenko <petr.fedchenko@gmail.com>
2018-06-21 16:53:52 +0300

    test2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RegistrerComponent } from './components/register/register.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrerComponent
+    RegisterComponent
   ],
   imports: [
     BrowserModule
